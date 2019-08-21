86b9647fdeaff63a2f46a02b6471144eaf7b0bcf
Andreas Justin <ajustin828@gmail.com>
2019-08-21 06:52:47 +0200

    Update Error.m

--- +util/Error.m
+++ +util/Error.m
@@ -1,11 +1,14 @@
 classdef Error
-% Error utility with identifier enumeration
-% 
+% Error identifier enumeration with utility function to create errors or throw errors
+%
 %% DESCRIPTION
-% An collection of commonly used error identifiers with utility functionality.
-% 
+% An collection of commonly used error identifiers with utility functionality for the creation "error" function or appending
+% errors with the "errorWithCause..." methods. If an output argument is provided the MException object is returned, otherwise
+% the created MException is thrown as caller.
+%
 %% VERSIONING
-%             Author: Andreas Justin
+%             Author: Andreas Justin, MO RS SC BG EN SSV TD-E
+%           Copyright (C) Siemens Mobility GmbH, 2018 - 2019 All Rights Reserved
 %      Creation date: 2018-06-21
 %             Matlab: 9.5, (R2018b)
 %  Required Products: -
@@ -17,13 +20,16 @@ classdef Error
 % V0.4 | 2018-10-31 | Martin Lechner      | new error type NOT_SUPPORTED_COMPILER
 % V0.5 | 2018-11-13 | Martin Lechner      | comments added to the different error categories
 % V1.0 | 2019-01-08 | Martin Lechner      | throw instead of throwAsCaller so that the root cause is also shown
+% V1.1 | 2019-04-25 | Martin Lechner      | throwAsCaller implemented for validation functions
+% V1.2 | 2019-04-26 | Martin Lechner      | improve docu, error Tags fixed
+% V2.0 | 2019-08-09 | Martin Lechner      | all error method throws the error as caller in the case of no output argument;
+%                                           throw and throwAsCaller methods removed, comments added to the methods
 %
-% See also 
+% See also
 %
 %% EXAMPLES
 %{
     % directly throws error
-    util.Error.INVALID_TYPE.throw("given class[=" + class("asdf") + "] is not supported")
     util.Error.INVALID_TYPE.error("given class[=" + class("asdf") + "] is not supported")
     
     % also returns an MException object
@@ -32,7 +38,13 @@ classdef Error
     % allows to add causes to error
     mException = MException("some:identifier", "some error message")
     util.Error.INVALID_TYPE.errorWithCause(mException, "Some user who is testing the given examples")
+
+    mException = MException("some:identifier", "some error message");
+    util.Error.INVALID_TYPE.errorWithCauseBefore(mException, "Some user who is testing the given examples with errorWithCauseBefore!")
     
+    mException = MException("some:identifier", "some error message");
+    util.Error.INVALID_TYPE.errorWithCauseAfter(mException, "Some user who is testing the given examples with errorWithCauseAfter!")
+
     % also allows to return the error
     mException = util.Error.INVALID_TYPE.errorWithCause(mException, "Some user who is testing the given examples")
 
@@ -47,21 +59,31 @@ classdef Error
 
 enumeration
     % for invalid types in inputs
-    INVALID_TYPE ("da:InvalidType")
-    % for invalid size of inputs or internal states
-    INVALID_SIZE ("da:InvalidSize")
-    INVALID_ARGUMENT ("da:invalidArgument")
-    % for invalid states in the calculation or methods
-    INVALID_STATE ("da:invalidState")
-    NOT_EQUAL ("da:NotEqual")
+    INVALID_TYPE("da:InvalidType")
+    
+    % for invalid size of input arguments or internal states
+    INVALID_SIZE("da:InvalidSize")
+    
+    % for invalid input arguments in functions or methods
+    INVALID_ARGUMENT("da:InvalidArgument")
+    
+    % for invalid states in the calculation of functions or methods
+    INVALID_STATE("da:InvalidState")
+    
+    % for invalid states where equal elements are expected but not available
+    NOT_EQUAL("da:NotEqual")
+    
     % function or parameter is defined but not supported to use (only if the implementation isn't finish)
     % If a function or parameter option isn't support anymore than use the DEPRECATED error instead.
-    NOT_SUPPORTED ("da:NotSupported")
+    NOT_SUPPORTED("da:NotSupported")
+    
     % for deprecated functions, methods or classes
     % Don't use this error for not yet implemented functions or parameter options!
-    DEPRECATED ("da:Deprecated")
+    DEPRECATED("da:Deprecated")
+    
     % work in progress, for not yet finished functions
-    WIP ("da.WIP")
+    WIP("da:WIP")
+    
     % error for not supported functions in standalone applications due to Matlab Compiler limitations (like opening Matlab
     % GUI's or calling unsupported functions)
     NOT_SUPPORTED_COMPILER("da:NotSupportedCompiler")
@@ -81,40 +103,73 @@ methods
         str = obj.identifier;
     end
 
-    function throw(obj, message, varargin)
-        mException = obj.error(message, varargin{:});
-        mException.throw();
-    end
-
     function mException = error(obj, message, varargin)
+        % Creates an MException and returns this object if an output argument is provided, otherwise the created error is
+        % thrown as caller.
+        % The error message is created from the given message string and the additionally arguments varargin like the Matlab
+        % error functions supports.
+        % The best solution for validate functions is to use the following pattern:
+        %       err = util.Error.INVALID_ARGUMENT.error("Reason");
+        %       err.throwAsCaller()
+        % If the error is throw directly also the validation function line is included in the stack trace!
+        %       util.Error.INVALID_ARGUMENT.error("Reason")
         mException = MException(obj.identifier.string(), message, varargin{:});
         if nargout < 1
-            mException.throw();
+            mException.throwAsCaller();
         end
     end
 
     function mException = errorWithCause(obj, baseException, message, varargin)
+        % Generates an error message with this created error as cause appended to the primary exception containing the
+        % primary cause and location of an error, specified as an MException object. If an output argument is provided the
+        % MException object is returned, otherwise the created MException is thrown as caller. The error message is created
+        % from the given message string and the additionally arguments varargin like the Matlab error functions supports.
+        % Useful in catch blocks to add information to the condition or situation in which the baseException happend. Same
+        % behaviour as "errorWithCauseAfter".
+        %
+        % Hint:
+        % In case of long stack traces the real cause expressed by this error message is shown belog the primary exception,
+        % so that the user doesn't have to scroll up to the first line of the stack trace.
         mException = obj.errorWithCauseAfter(baseException, message, varargin{:});
         if nargout < 1
-            mException.throw();
+            mException.throwAsCaller();
         end
     end
 
     function mException = errorWithCauseAfter(obj, baseException, message, varargin)
+        % Generates an error message with this created error as cause appended to the primary exception containing the
+        % primary cause and location of an error, specified as an MException object.If an output argument is provided the
+        % MException object is returned, otherwise the created MException is thrown as caller. The error message is created
+        % from the given message string and the additionally arguments varargin like the Matlab error functions supports.
+        % Useful in catch blocks to add information to the condition or situation in which the baseException happend.
+        %
+        % Hint:
+        % In case of long stack traces the real cause expressed by this error message is shown below the primary exception,
+        % so that the user doesn't have to scroll up to the first line of the stack trace.
         cause = obj.error(message, varargin{:});
         mException = addCause(baseException, cause);
         if nargout < 1
-            mException.throw();
+            mException.throwAsCaller();
         end
     end
 
     function mException = errorWithCauseBefore(obj, baseException, message, varargin)
+        % Generates an error message with this created error as the primary exception appended with the baseException
+        % containing the real cause and location of an error, specified as an MException object. If an output argument is
+        % provided the MException object is returned, otherwise the created MException is thrown as caller. The error message
+        % is created from the given message string and the additionally arguments varargin like the Matlab error functions
+        % supports.
+        % Useful in catch blocks to add information to the condition or situation in which the baseException happend.
+        %
+        % Hint:
+        % In case of long stack traces the real cause expressed by this error message is shown above (first line) the primary
+        % exception, so that the user doesn't have to scroll up to the first line of the stack trace.
         cause = obj.error(message, varargin{:});
         mException = addCause(cause, baseException);
         if nargout < 1
-            mException.throw();
+            mException.throwAsCaller();
         end
     end
 end
 
-end
+end     % classdef
